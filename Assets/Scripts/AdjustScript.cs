8febe0f4403aeb1f6d7b7ca8a761a2e2d7d6c5c5
CCisGG <hgengcmu@gmail.com>
2018-04-15 10:48:43 -0700

    Save data on change scene, using a game controller

---
+++ Assets/Scripts/AdjustScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustScript : MonoBehaviour {
+
+	void OnGUI() {
+		if (GUI.Button (new Rect (10, 100, 100, 30), "Health UP")) {
+			GameController.gameController.health += 10;
+		}
+		if (GUI.Button (new Rect (10, 140, 100, 30), "Health Down")) {
+			GameController.gameController.health -= 10;
+		}
+		if (GUI.Button (new Rect (10, 180, 100, 30), "Experience UP")) {
+			GameController.gameController.experience += 10;
+		}
+		if (GUI.Button (new Rect (10, 220, 100, 30), "Experience Down")) {
+			GameController.gameController.health -= 10;
+		}
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * GameController: Save data
+ *
+ */
+
+public class GameController : MonoBehaviour {
+
+	public static GameController gameController;
+
+	public float health;
+	public float experience;
+
+	// Use this for initialization
+	void Awake () {
+		if (gameController == null) {
+			DontDestroyOnLoad (gameObject);
+			gameController = this;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
