60cdb9a14f7a8da74d95ce875fb5593bf97c8990
CCisGG <hgengcmu@gmail.com>
2018-04-15 16:56:38 -0700

    Show health and Exp top left. Keep remained when switch the scene

--- Assets/Scripts/AdjustScript.cs
+++ Assets/Scripts/AdjustScript.cs
@@ -11,11 +11,18 @@ public class AdjustScript : MonoBehaviour {
 		if (GUI.Button (new Rect (10, 140, 100, 30), "Health Down")) {
 			GameController.gameController.health -= 10;
 		}
-		if (GUI.Button (new Rect (10, 180, 100, 30), "Experience UP")) {
+		if (GUI.Button (new Rect (10, 180, 100, 30), "Exp UP")) {
 			GameController.gameController.experience += 10;
 		}
-		if (GUI.Button (new Rect (10, 220, 100, 30), "Experience Down")) {
-			GameController.gameController.health -= 10;
+		if (GUI.Button (new Rect (10, 220, 100, 30), "Exp Down")) {
+			GameController.gameController.experience -= 10;
+		}
+
+		if (GUI.Button (new Rect (10, 300, 100, 30), "Save")) {
+			GameController.gameController.Save();
+		}
+		if (GUI.Button (new Rect (10, 340, 100, 30), "Load")) {
+			GameController.gameController.Load();
 		}
 	}
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
 
 /*
  * GameController: Save data
@@ -14,7 +18,6 @@ public class GameController : MonoBehaviour {
 	public float health;
 	public float experience;
 
-	// Use this for initialization
 	void Awake () {
 		if (gameController == null) {
 			DontDestroyOnLoad (gameObject);
@@ -25,8 +28,36 @@ public class GameController : MonoBehaviour {
 
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	public void Save() {
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Create (Application.persistentDataPath + "/playerInfo.dat");
+
+		PlayerData data = new PlayerData ();
+		data.health = health;
+		data.experience = experience;
+
+		bf.Serialize (file, data);
+		file.Close();
 	}
+
+	public void Load() {
+		if (File.Exists(Application.persistentDataPath + "/playerInfo.dat")) {
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = File.Open (Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+			PlayerData data = (PlayerData) bf.Deserialize (file);
+			file.Close();
+
+			health = data.health;
+			experience = data.experience;
+		}
+
+
+	}
+}
+
+
+[Serializable]
+class PlayerData {
+	public float health;
+	public float experience;
 }
