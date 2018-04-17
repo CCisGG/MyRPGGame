50b21f2b1bd9374b7ce64a803b2b8bd2c4318391
CCisGG <hgengcmu@gmail.com>
2018-04-16 22:02:27 -0700

    Wait to revert

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType {Sword, Health}; 
+
+
+public class Item : MonoBehaviour {
+
+	public ItemType type;
+
+	public Sprite sprite;
+
+	public int maxSize;
+
+	void Use() {
+		switch (type) {
+			case ItemType.Health:
+				Debug.Log ("health item");
+				break;
+			case ItemType.Sword:
+				Debug.Log ("Sword item");
+				break;
+		}
+	}
+}
