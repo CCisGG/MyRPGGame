66ebf2b66a2c5aeaa9f4a5e77211f87ec57cfd3e
CCisGG <hgengcmu@gmail.com>
2018-04-17 22:37:27 -0700

    Add basic inventory functions.

--- Assets/Scripts/InventoryScripts/Item.cs
+++ Assets/Scripts/InventoryScripts/Item.cs
@@ -2,24 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ItemType {Sword, Health}; 
+public enum ItemType {Weapon, Health, Mana}; 
 
 
 public class Item : MonoBehaviour {
 
 	public ItemType type;
 
-	public Sprite sprite;
+	public Sprite spriteNeutral;
+
+	public Sprite spriteHighlighted;
 
 	public int maxSize;
 
-	void Use() {
+	public void Use() {
 		switch (type) {
 			case ItemType.Health:
 				Debug.Log ("health item");
+				GameController.gameController.health += 10;
 				break;
-			case ItemType.Sword:
-				Debug.Log ("Sword item");
+			case ItemType.Weapon:
+				Debug.Log("Weapon item");
 				break;
 		}
 	}
