db45103a05163dfc945ed1ab0aa7d3739acfb80f
CCisGG <hgengcmu@gmail.com>
2018-04-18 23:19:26 -0700

    Add function: tab Space to pick up item

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class PlayerController : MonoBehaviour {
 
@@ -16,12 +17,37 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		// Accept the move signal from keyboard "input", and move the position
 		this.transform.position = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0 ) + this.transform.position;
+	
+		DetectSpace ();
 	}
 
-	// Pick up items on collision.
-	void OnTriggerEnter2D(Collider2D other) {
-		if (other.CompareTag ("Item")) {
-			inventory.AddItem (other.GetComponent<Item>());
+	// Pick up items on Tab Space.
+	private bool DetectSpace() {
+		if (Input.GetKey(KeyCode.Space)) {
+			GameObject item = findClosestObject ("Item");
+
+			if (item != null) 
+				inventory.AddItem (item.GetComponent<Item>());
+			return true;
 		}
+		return false;
+	}
+
+	public GameObject findClosestObject(string tag) {
+		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag (tag);
+		GameObject closestObject = null;
+		float minDist = 5;
+
+		foreach (GameObject obj in gameObjects) {
+			float dist = Vector3.Distance (obj.transform.position, gameObject.transform.position);
+
+			if (dist < minDist ) {
+				minDist = dist;
+				closestObject = obj;
+			}
+		}
+
+
+		return closestObject;
 	}
 }
