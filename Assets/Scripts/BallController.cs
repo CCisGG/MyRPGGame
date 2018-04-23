529eda95df9c0f9f4cc802297940a36d6014666a
Xuan Yu <xy966@nyu.edu>
2018-04-23 15:45:30 -0400

    add monster attack player

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Hurt player.
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.CompareTag ("Player")) {
+			if (GameController.gameController.health >= 10) {
+				GameController.gameController.health -= 10;
+			} else {
+				GameController.gameController.health = 0;
+			}
+			GameObject.Destroy(gameObject);
+		}
+		
+	}
+}
