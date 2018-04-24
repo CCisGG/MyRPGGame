3c529a1a70842964359ce62fba3ef35f767784b6
CCisGG <hgengcmu@gmail.com>
2018-04-23 20:48:48 -0700

    Fix the bug for pick up item.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour {
 
 	public InventoryController inventory;
 
+	private Collider2D overlapItem;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -17,14 +19,25 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		// Accept the move signal from keyboard "input", and move the position
 		this.transform.position = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0 ) + this.transform.position;
+	
+		// Only allow one single item in specific range
+		DetectPickUp ();
 	}
 
-	void OnTriggerStay2D (Collider2D other) {
-		if (!other.CompareTag ("Item"))
+	void OnTriggerEnter2D (Collider2D other) {
+		overlapItem = other;
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		overlapItem = null;
+	}
+
+	public void DetectPickUp() {
+		if (overlapItem == null || !overlapItem.CompareTag ("Item"))
 			return;
 		if (Input.GetKeyDown(KeyCode.Space)) {
-			Debug.Log ("Picking up " + other.GetComponent<Item> ());
-			inventory.AddItem (other.GetComponent<Item> ());
+			Debug.Log ("Picking up " + overlapItem.GetComponent<Item> ());
+			inventory.AddItem (overlapItem.GetComponent<Item> ());
 			//			GameObject.Destroy (item);
 		}
 	}
