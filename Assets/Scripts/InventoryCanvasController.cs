e94432d48a2f51cc755717509793fe8fdd0a74b1
CCisGG <hgengcmu@gmail.com>
2018-05-01 18:02:15 -0700

    Fix bugs on inventory

---
+++ Assets/Scripts/InventoryCanvasController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryCanvasController : MonoBehaviour {
+
+	private CanvasScaler scaler;
+
+	public static InventoryCanvasController canvasController;
+
+	void Awake () {
+		if (canvasController == null) {
+			DontDestroyOnLoad (gameObject);
+			canvasController = this;
+		} else if (canvasController != this) {
+			Destroy (gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		scaler = GetComponent<CanvasScaler> ();
+		scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+	}
+}
