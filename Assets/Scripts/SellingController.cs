2f23287d48833b5ff38abb7365e0b392c9690b49
CCisGG <hgengcmu@gmail.com>
2018-05-02 21:25:20 -0700

    Refine the selling system

---
+++ Assets/Scripts/SellingController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SellingController : MonoBehaviour {
+
+	SellingController sellingController;
+
+	void Awake () {
+		if (sellingController == null) {
+			DontDestroyOnLoad (gameObject);
+			sellingController = this;
+		} else if (sellingController != this) {
+			Destroy (gameObject);
+		}
+
+	}
+
+	public void Sell() {
+		Slot fromSlot = InventoryController.GetInventoryController ().FromSlot;
+		if (!fromSlot.GetCurrentItem ().IsSoldable) {
+			Debug.Log ("Item " + fromSlot.GetCurrentItem() + " cannot be sold");
+			return;
+		}
+		GameController.gameController.gold += fromSlot.GetCurrentItem().price;
+		fromSlot.ClearSlot();
+		InventoryController.GetInventoryController ().FromSlot = null;
+	}
+}
