248a62bce1dfd404934b62c4717ca3c07f73e74d
CCisGG <hgengcmu@gmail.com>
2018-05-14 21:43:37 -0700

    Add level machanism

---
+++ Assets/Scripts/Controllers/LevelController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LevelController : MonoBehaviour {
+    
+    private static LevelController levelController;
+
+    private static int level;
+
+    public int Level {
+        get { return level; }
+        set { level = value; }
+    }
+
+    public static LevelController Controller
+    {
+        get { return levelController; }
+    }
+
+    private void Initialize()
+    {
+        if (levelController == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            levelController = this;
+        }
+        else if (levelController != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+	void Awake()
+	{
+        Initialize();
+	}
+
+	void Start()
+	{
+        level = 1;
+	}
+}
---
+++ Assets/Scripts/MineSceneManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MineSceneManager : MonoBehaviour {
+
+	// Change scene on collision
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.CompareTag ("Player")) {
+            FindObjectOfType<LevelController>().Level++;
+            Debug.Log("Level #" + FindObjectOfType<LevelController>().Level);
+			SceneManager.LoadScene ("MineScene");
+		}
+	}
+}
